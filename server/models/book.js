2cd406cd69aa31187f126155cb66ee02f7bc87c2
adydams <adydams@gmail.com>
2017-11-07 14:19:47 +0100

    modify book model

--- server/models/book.js
+++ server/models/book.js
@@ -1,11 +1,28 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Book = sequelize.define('Book', {
-    bookTitle: DataTypes.STRING,
-    author: DataTypes.STRING,
-    isbn: DataTypes.STRING,
-    bookSummary: DataTypes.STRING,
-    quantity: DataTypes.INTEGER
+    bookTitle: {
+      type:DataTypes.STRING,
+      allowNull: false, 
+      unique: true
+      },
+    author: {
+      type:DataTypes.STRING,
+      allowNull: false
+      },
+    isbn:{
+      type:DataTypes.STRING,
+      allowNull: false, 
+      unique: true
+      },
+    bookSummary: {
+      type:DataTypes.STRING,
+      allowNull: false
+      },
+    quantity: {
+      type:DataTypes.STRING,
+      allowNull: false
+      },
   }, {
     classMethods: {
       associate: (models)=> {
